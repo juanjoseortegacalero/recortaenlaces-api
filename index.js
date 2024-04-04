b5fef9d14a26472757c7f02184cf13d03c3ca69f
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-04-04 16:44:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://recortaenlaces.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
