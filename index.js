6a38fbeda007e03cbf9fad5685cadbe216e1f4fc
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-04-05 16:14:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/', require('./routes/authRoutes'))
 
-const port = 8080;
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
