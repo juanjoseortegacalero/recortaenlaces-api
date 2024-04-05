45e76ffa7af76a49ed844471c309c15ff72b8663
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-04-05 13:48:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,42 @@ const dotenv = require('dotenv').config()
 const cors = require('cors')
 const {mongoose} = require ('mongoose')
 const cookieParser = require ('cookie-parser')
+const router = express.Router();
+const { test, registerUser, loginUser, getProfile, logOut, cortarEnlace, verEnlace, reDirect, ultimoEnlace} = require('./controllers/authControllers')
 const app= express();
 
 
 //Conection database
-mongoose.connect(process.env.MONGO_URL)
-.then(db => console.log('Db is connected'))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URL) .then(()=> console.log('Database Connected')) .catch(()=> console.log('Database Connected', err))
 
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://recortaenlaces.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
-app.use('/', require('./routes/authRoutes'))
+router.use(
+    cors({
+        credentials: true,
+        origin: 'https://recortaenlaces.onrender.com'
+        
+    })
+)
 
-const port = 8080;
+
+router.get('/', test)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/profile', getProfile)
+router.get('/logout', logOut)
+router.post('/corta', cortarEnlace)
+router.get('/verEnlace', verEnlace)
+router.get('/ultimoEnlace', ultimoEnlace)
+router.post('/:id', reDirect)
+
+
+module.exports = router
+
+const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
