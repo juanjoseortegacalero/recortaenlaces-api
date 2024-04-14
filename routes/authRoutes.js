ff958aded20d4c8fe434b9062b53642306b4af2b
Juan José Ortega <81860658+juanjoseortegacalero@users.noreply.github.com>
2024-04-14 18:32:45 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ const { test, registerUser, loginUser, getProfile, logOut, cortarEnlace, verEnla
 router.use(
     cors({
         credentials: true,
-        origin: process.env.FRONTEND_URL
+        origin: 'https://recortaenlaces.onrender.com'
         
     })
 )
